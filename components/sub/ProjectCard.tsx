1bdd139f129f7d663cf9ea937efdc3106fe80e3b
Onkar Shinde <onkarshinde499@gmail.com>
2025-11-11 12:25:39 +0530

    fix: resolve TypeScript errors in ProjectCard and StarBackground components

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import Image from 'next/image'
 
-const ProjectCard = ({src , title , description } : Props) => {
+interface Props {
+  src: string;
+  title: string;
+  description: string;
+}
+
+const ProjectCard = ({ src, title, description }: Props) => {
   return (
     <div className='relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]'>
         <Image 
-        src={src}
-        alt={title}
-        width={1000}
-        height={1000}
-        className='w-full object-contain'
+          src={src}
+          alt={title}
+          width={1000}
+          height={1000}
+          className='w-full object-contain'
         />
 
         <div className='relative p-4'>
